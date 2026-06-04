16f46646716b62a69ffd04da23a3fb0fbc39eb43
keerthy <2022sp140@univ.jfn.ac.lk>
2026-06-04 16:54:19 +0530

    color change

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -113,7 +113,7 @@ function Login() {
               <Link to="/signup">Signup</Link>
             </div>
           </form>
-          <button type="button" className="login-btn" onClick={handleBack}> Home</button>
+          <button type="button" className="back-btn" onClick={handleBack}> Home</button>
         </div>
       </div>
     </div>
