5cd74ee93e71c04c182dd0a29f84431b6c4b8a39
Piriya <kajappiriya222@gmail.com>
2026-06-20 17:00:51 +0530

    side bar issue in procument is fixed

--- client/src/components/procurement/ProcurementWorkspace.jsx
+++ client/src/components/procurement/ProcurementWorkspace.jsx
@@ -1,17 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import ProcurementTopNav from './ProcurementTopNav';
-import DashboardLayout from '../DashboardLayout';
 import '../../styles/ProcurementWorkspace.css';
 
 export default function ProcurementWorkspace() {
   return (
-    <DashboardLayout active="procurement">
-      <div className="procurement-workspace">
-        <ProcurementTopNav />
-        <div className="procurement-workspace-content">
-          <Outlet />
-        </div>
+    <div className="procurement-workspace">
+      <ProcurementTopNav />
+      <div className="procurement-workspace-content">
+        <Outlet />
       </div>
-    </DashboardLayout>
+    </div>
   );
 }
